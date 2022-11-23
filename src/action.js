e32cfdd25a2cc269430e991ed66bca74e17138f7
David Precious <davidp@preshweb.co.uk>
2022-11-23 22:20:34 +0000

    Support multiple lock files and autodetection
    In some monorepo-like repos I have multiple package-lock.json files at
    different levels/locations; I'd like to be able to check them all.
    
    Being able to easily specify a list of paths to package-lock.json files
    would be useful, so I've added support for that, but also added the
    ability to set `path` to `auto` to automatically find all
    `package-lock.json` files within the repository.

--- src/action.js
+++ src/action.js
@@ -5,6 +5,7 @@ const PackageLockParser =
 const fs = require('fs')
 const { Base64 } = require('js-base64')
 const path = require('path')
+const jetpack = require("fs-jetpack")
 
 const { STATUS, countStatuses, diffLocks } = require('./utils')
 const { createTable, createSummary } = require('./comment')
@@ -32,6 +33,11 @@ const getCommentId = async (octokit, oktokitParams, issueNumber, commentHeader) 
 const getBasePathFromInput = (input) =>
   input.lastIndexOf('/') ? input.substring(0, input.lastIndexOf('/')) : ''
 
+const findLockfiles = (dir) => {
+  return jetpack.find(dir, { matching: "package-lock.json" });
+
+}
+
 const run = async () => {
   try {
     const octokit = getOctokit(getInput('token', { required: true }))
@@ -46,124 +52,140 @@ const run = async () => {
       throw Error('💥 Cannot find the PR data in the workflow context, aborting!')
     }
 
-    const { ref } = context.payload.pull_request.base
-    const { default_branch } = context.payload.repository
-
-    const baseBranch = ref || default_branch
-    debug('Base branch: ' + baseBranch)
-
-    const lockPath = path.resolve(process.cwd(), inputPath)
-
-    if (!fs.existsSync(lockPath)) {
-      throw Error(
-        '💥 The code has not been checkout or the lock file does not exist in this PR, aborting!'
-      )
+    // Work out which package-lock.json file(s) to operate on; inputPath can
+    // be set to a semicolon-separated list, or the magic value "auto",
+    // which means we should search for package-lock.json files
+    let lockfilesToProcess = [];
+    if (inputPath == "auto") {
+      lockfilesToProcess = findLockfiles(process.cwd())
+    } else {
+      lockfilesToProcess = inputPath.split(/\s*;\s*/)
     }
 
-    const content = fs.readFileSync(lockPath, { encoding: 'utf8' })
-    const updatedLock = packageLockParser.parseLockFile(content)
+    console.log(`Going to process lockfiles: `, lockfilesToProcess)
 
-    const oktokitParams = { owner, repo }
-    debug('Oktokit params: ' + JSON.stringify(oktokitParams))
+    for (const lockfile of lockfilesToProcess) {
+        debug(`Processing lockfile ${lockfile}`)
 
-    const basePath = getBasePathFromInput(inputPath)
-    debug('Base lockfile path: ' + basePath)
+        const { ref } = context.payload.pull_request.base
+        const { default_branch } = context.payload.repository
 
-    const baseTree = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{branch}:{path}', {
-      ...oktokitParams,
-      branch: baseBranch,
-      path: basePath,
-    })
+        const baseBranch = ref || default_branch
+        debug('Base branch: ' + baseBranch)
 
-    if (!baseTree || !baseTree.data || !baseTree.data.tree) {
-      throw Error('💥 Cannot fetch repository base branch tree, aborting!')
-    }
+        const lockPath = path.resolve(process.cwd(), inputPath)
 
-    const baseLockSHA = baseTree.data.tree.filter((file) => file.path === 'package-lock.json')[0]
-      .sha
-    debug('Base lockfile SHA: ' + baseLockSHA)
+        if (!fs.existsSync(lockPath)) {
+        throw Error(
+            '💥 The code has not been checkout or the lock file does not exist in this PR, aborting!'
+        )
+        }
 
-    const baseLockData = await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', {
-      ...oktokitParams,
-      file_sha: baseLockSHA,
-    })
+        const content = fs.readFileSync(lockPath, { encoding: 'utf8' })
+        const updatedLock = packageLockParser.parseLockFile(content)
 
-    if (!baseLockData || !baseLockData.data || !baseLockData.data.content) {
-      throw Error('💥 Cannot fetch repository base lock file, aborting!')
-    }
+        const oktokitParams = { owner, repo }
+        debug('Oktokit params: ' + JSON.stringify(oktokitParams))
 
-    const baseLock = packageLockParser.parseLockFile(Base64.decode(baseLockData.data.content))
-    const lockChanges = diffLocks(baseLock, updatedLock)
-    const lockChangesCount = Object.keys(lockChanges).length
+        const basePath = getBasePathFromInput(inputPath)
+        debug('Base lockfile path: ' + basePath)
 
-    const commentHeader = '## `' + inputPath + '` changes'
-    const commentId = updateComment
-      ? await getCommentId(octokit, oktokitParams, number, commentHeader)
-      : undefined
-    debug('Bot comment ID: ' + commentId)
+        const baseTree = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{branch}:{path}', {
+        ...oktokitParams,
+        branch: baseBranch,
+        path: basePath,
+        })
 
-    if (lockChangesCount) {
-      let diffsTable = createTable(lockChanges)
+        if (!baseTree || !baseTree.data || !baseTree.data.tree) {
+        throw Error('💥 Cannot fetch repository base branch tree, aborting!')
+        }
 
-      if (diffsTable.length >= 64000) {
-        diffsTable = createTable(lockChanges, true)
-      }
+        const baseLockSHA = baseTree.data.tree.filter((file) => file.path === 'package-lock.json')[0]
+        .sha
+        debug('Base lockfile SHA: ' + baseLockSHA)
 
-      const collapsed = lockChangesCount >= collapsibleThreshold
-      const changesSummary = collapsed ? '### Summary\n' + createSummary(lockChanges) : ''
+        const baseLockData = await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', {
+        ...oktokitParams,
+        file_sha: baseLockSHA,
+        })
 
-      const body =
-        commentHeader +
-        '\n' +
-        changesSummary +
-        '\n' +
-        '<details' +
-        (collapsed ? '' : ' open') +
-        '>\n' +
-        '<summary>Click to toggle table visibility</summary>\n<br/>\n\n' +
-        diffsTable +
-        '\n\n' +
-        '</details>'
+        if (!baseLockData || !baseLockData.data || !baseLockData.data.content) {
+        throw Error('💥 Cannot fetch repository base lock file, aborting!')
+        }
 
-      if (updateComment) {
-        if (commentId) {
-          await octokit.rest.issues.updateComment({
-            ...oktokitParams,
-            comment_id: commentId,
-            body,
-          })
+        const baseLock = packageLockParser.parseLockFile(Base64.decode(baseLockData.data.content))
+        const lockChanges = diffLocks(baseLock, updatedLock)
+        const lockChangesCount = Object.keys(lockChanges).length
+
+        const commentHeader = '## `' + inputPath + '` changes'
+        const commentId = updateComment
+        ? await getCommentId(octokit, oktokitParams, number, commentHeader)
+        : undefined
+        debug('Bot comment ID: ' + commentId)
+
+        if (lockChangesCount) {
+        let diffsTable = createTable(lockChanges)
+
+        if (diffsTable.length >= 64000) {
+            diffsTable = createTable(lockChanges, true)
+        }
+
+        const collapsed = lockChangesCount >= collapsibleThreshold
+        const changesSummary = collapsed ? '### Summary\n' + createSummary(lockChanges) : ''
+
+        const body =
+            commentHeader +
+            '\n' +
+            changesSummary +
+            '\n' +
+            '<details' +
+            (collapsed ? '' : ' open') +
+            '>\n' +
+            '<summary>Click to toggle table visibility</summary>\n<br/>\n\n' +
+            diffsTable +
+            '\n\n' +
+            '</details>'
+
+        if (updateComment) {
+            if (commentId) {
+            await octokit.rest.issues.updateComment({
+                ...oktokitParams,
+                comment_id: commentId,
+                body,
+            })
+            } else {
+            await octokit.rest.issues.createComment({
+                ...oktokitParams,
+                issue_number: number,
+                body,
+            })
+            }
         } else {
-          await octokit.rest.issues.createComment({
+            await octokit.rest.issues.createComment({
             ...oktokitParams,
             issue_number: number,
             body,
-          })
+            })
         }
-      } else {
-        await octokit.rest.issues.createComment({
-          ...oktokitParams,
-          issue_number: number,
-          body,
-        })
-      }
-    } else {
-      if (updateComment && commentId) {
-        await octokit.rest.issues.deleteComment({
-          ...oktokitParams,
-          comment_id: commentId,
-        })
-      }
-    }
+        } else {
+        if (updateComment && commentId) {
+            await octokit.rest.issues.deleteComment({
+            ...oktokitParams,
+            comment_id: commentId,
+            })
+        }
+        }
 
-    if (countStatuses(lockChanges, STATUS.DOWNGRADED)) {
-      warning('Dependency downgrade detected!')
+        if (countStatuses(lockChanges, STATUS.DOWNGRADED)) {
+        warning('Dependency downgrade detected!')
 
-      if (failOnDowngrade) {
-        throw Error('🚨 Dependency downgrade with `failOnDowngrade` flag set, failing the action!')
-      }
+        if (failOnDowngrade) {
+            throw Error('🚨 Dependency downgrade with `failOnDowngrade` flag set, failing the action!')
+        }
+        }
+    } catch (error) {
+        setFailed(error.message)
     }
-  } catch (error) {
-    setFailed(error.message)
   }
 }
 
